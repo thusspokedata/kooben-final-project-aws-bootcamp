85609c4910c28e3fda05b7d143d3ef01afe9525a
thusspokedata <thusspokedata@protonmail.com>
2025-02-18 19:58:44 +0100

    fix: remove profile from providers

--- providers.tf
+++ providers.tf
@@ -15,9 +15,6 @@ terraform {
 
 provider "aws" {
   region = "eu-central-1"
-  profile = "kooben"
-#   access_key = var.access_key
-#   secret_key = var.secret_key
   default_tags {
     tags = var.tags
   }
