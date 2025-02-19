f3803646a3492d3dd00083700140fe3a03daf44e
thusspokedata <thusspokedata@protonmail.com>
2025-02-19 15:21:06 +0100

    refactor(s3): improve S3 module outputs organization

--- infra/modules/S3/output.tf
+++ infra/modules/S3/output.tf
@@ -1,7 +1,14 @@
-output "bucket_name" {
-  value = aws_s3_bucket.kooben_storage_bucket.id
+output "bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.kooben_storage_bucket.arn
 }
 
-output "bucket_arn" {
-  value = aws_s3_bucket.kooben_storage_bucket.arn
+output "bucket_id" {
+  description = "The ID of the S3 bucket"
+  value       = aws_s3_bucket.kooben_storage_bucket.id
+}
+
+output "bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.kooben_storage_bucket.bucket
 }
