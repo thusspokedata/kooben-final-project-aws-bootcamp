faa46d6cc25c2d227f8086590165ef3a9ab019e1
thusspokedata <thusspokedata@protonmail.com>
2025-02-20 16:05:39 +0100

    fix(vpc): update outputs for multi-az private subnets

--- infra/modules/networking/modules/vpc/outputs.tf
+++ infra/modules/networking/modules/vpc/outputs.tf
@@ -8,7 +8,10 @@ output "public_subnet_id" {
   value       = aws_subnet.kooben_public_subnet.id
 }
 
-output "private_subnet_id" {
-  description = "ID of the private subnet"
-  value       = aws_subnet.kooben_private_subnet.id
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = [
+    aws_subnet.kooben_private_subnet_1.id,
+    aws_subnet.kooben_private_subnet_2.id
+  ]
 } 
