5ecdb78099cb33bef65ab1cd63654a9d411541ea
thusspokedata <thusspokedata@protonmail.com>
2025-02-20 16:07:04 +0100

    fix(vpc): update outputs for multi-az private subnets

--- infra/modules/networking/outputs.tf
+++ infra/modules/networking/outputs.tf
@@ -8,7 +8,7 @@ output "public_subnet_id" {
   value       = module.vpc.public_subnet_id
 }
 
-output "private_subnet_id" {
-  description = "ID of the private subnet"
-  value       = module.vpc.private_subnet_id
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = module.vpc.private_subnet_ids
 } 
