a904aff1bb0b82d6aca3d2714b6b65aebcd64308
thusspokedata <thusspokedata@protonmail.com>
2025-02-20 15:53:56 +0100

    fix(vpc): remove flow_log_destination_id variable and references

--- infra/modules/networking/main.tf
+++ infra/modules/networking/main.tf
@@ -2,23 +2,14 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-module "vpc" {
-  source = "./modules/vpc"
-
-  vpc_cidr            = var.vpc_cidr
-  public_subnet_cidr  = var.public_subnet_cidr
-  private_subnet_cidr = var.private_subnet_cidr
-  sufix              = var.sufix
-  tags               = var.tags
-}
 
 module "routing" {
   source = "./modules/routing"
 
   vpc_id           = module.vpc.vpc_id
   public_subnet_id = module.vpc.public_subnet_id
-  sufix           = var.sufix
-  tags            = var.tags
+  sufix            = var.sufix
+  tags             = var.tags
 }
 
 # Create KMS key for CloudWatch Logs encryption
