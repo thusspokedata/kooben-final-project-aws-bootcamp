ced2e37f3d22d5de3f1999fde22b9fcdd26e7c70
thusspokedata <thusspokedata@protonmail.com>
2025-02-27 11:11:05 +0100

    fix: correct route53 zone resource name in outputs

--- infra/modules/route53/outputs.tf
+++ infra/modules/route53/outputs.tf
@@ -1,9 +1,9 @@
 output "zone_id" {
-  value = aws_route53_zone.main.zone_id
+  value = aws_route53_zone.domain_zone.zone_id
 }
 
 output "name_servers" {
-  value = aws_route53_zone.main.name_servers
+  value = aws_route53_zone.domain_zone.name_servers
 }
 
 output "frontend_url" {
